20dbbc10ffeb9b068b17ab916058cb3bdc119cd1
vijaynallagatla <vijay.nallagatla@sharpershape.com>
2023-02-08 16:55:21 +0200

    Update the examples

--- examples/sample/main.go
+++ examples/sample/main.go
@@ -6,13 +6,15 @@ import (
 )
 
 type Example struct {
-	Name string `default:"John Doe"`
-	Age  int    `default:"30"`
+	Name       string `default:"John Doe"`
+	Age        int    `default:"30"`
+	Profession string `default:"Engineer"`
 }
 
 func main() {
 	example := Example{}
 	BindDefault(&example)
-	fmt.Println(example.Name) // Output: John Doe
-	fmt.Println(example.Age)  // Output: 30
+	fmt.Println(example.Name)       // Output: John Doe
+	fmt.Println(example.Age)        // Output: 30
+	fmt.Println(example.Profession) // Output: Engineer
 }
